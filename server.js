ad68976fee58244709c016b03e999966ce55e051
Glitch (file-metadata-injashkin) <none>
2020-04-12 15:11:22 +0000

    Edit READMY

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
 // require and use "multer"...
+const multer = require("multer");
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(multer({dest:"uploads"}).single("upfile"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get("/hello", function(req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
 });
